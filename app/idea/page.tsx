f0e269a9615b780a9415c613e46488f75ba99824
Manav Bansal <bansalmanav39@gmail.com>
2025-10-22 14:36:42 -0700

    Added basic structure for the project

---
+++ app/idea/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      idea page
+    </div>
+  )
+}
+
+export default page
---
+++ app/team/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      team page
+    </div>
+  )
+}
+
+export default page
